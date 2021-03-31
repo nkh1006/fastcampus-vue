c66e5b7628957d8918045de5dbe36aad0173202a
NANGGI <mqsora@naver.com>
2021-03-31 23:12:45 +0900

    Add files via upload

---
+++ 13_vue/app.js
@@ -0,0 +1,20 @@
+const vm = new Vue({
+   el: '#app',
+   data: {
+      todos: [
+         { title: '아침먹기' },
+         { title: '점심먹기' },
+         { title: '저녁먹기' }
+      ],
+      heropy: {
+         name: 'HEROPY',
+         age: 35
+      }
+   },
+   methods: {
+      pushTodo() {
+         // this.todos[3] = { title: '야식먹기' }
+         this.todos.push({ title: '야식 먹기' })
+      }
+   }
+});
